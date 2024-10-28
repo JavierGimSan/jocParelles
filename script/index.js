97a5c29503c59f16f76d8b534fa5d6874afd0c31
JavierGimSan <javi.gimsa@gmail.com>
2024-10-28 20:23:20 +0100

    Primer commit

---
+++ script/index.js
@@ -0,0 +1,45 @@
+//DECLARAR OBJECTES
+
+const btnPartida = document.getElementById("btn-partida");
+const btnBorrar = document.getElementById("btn-borrar");
+const nomJugadorObj = document.getElementById("nom-jugador");
+const infoNavegadorObj = document.getElementById("info-navegador");
+const infoUrlObj = document.getElementById("info-url");
+
+//DECLARAR EVENTS
+
+btnPartida.addEventListener("click", comencarPartida);
+btnBorrar.addEventListener("click", borrarPartida);
+
+//DECLARAR VARIABLES I CONSTANTS
+let win
+
+//FUNCIONALITAT
+
+function comencarPartida(){
+    if(nomJugadorObj.value){
+        
+        win = window.open("joc.html");
+    }
+    else{
+        alert("Has d'informar un nom");
+    }     
+}
+
+function borrarPartida(){
+    win.close("joc.html");
+}
+
+function informacioNavegador(){
+    const color = "orange";
+    const usrAgent = navigator.userAgent;
+    infoNavegadorObj.textContent = usrAgent;
+}
+
+function infoURL(){
+    const url = location.origin;
+    infoUrlObj.textContent = url;
+}
+
+informacioNavegador();
+infoURL();
