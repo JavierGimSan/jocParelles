680470c3afe35f7bc93b5a5813c976c417513398
JavierGimSan <javi.gimsa@gmail.com>
2024-11-14 01:02:18 +0100

    Puntuació de la partida

---
+++ script/instruccions.js
@@ -0,0 +1,7 @@
+const botoTancaInstruccions = document.getElementById('btn-tanca-instruccions');
+
+botoTancaInstruccions.addEventListener('click', tancarInstruccions);
+
+function tancarInstruccions(){
+    //ARREGLAR CIERRE DE INSTRUCCIONES (sessionStorage??)
+}
--- script/joc.js
+++ script/joc.js
@@ -1,6 +1,9 @@
 //DECLARAR OBJECTES
 const nomObj = document.getElementById("nom-partida");
 const containerQuadrats = document.getElementById("quadrats-container");
+const puntsPartidaObj = document.getElementById("punts-partida");
+const numPuntsObj = document.getElementById("num-punts"); //Puntuació de la partida.
+
 
 //DECLARAR VARIABLES I CONSTANTS
 const NUMCASELLES = 20;
@@ -26,7 +29,7 @@ for(i=0; i<NUMCASELLES; i++){
     quadrat.addEventListener('click', comprovarParella);
 }
 
-function comprovarParella(){
+function comprovarParella(){ //Si son iguals, es posen verd. Si no, vermelles durant un segon i després es tornen a amagar.
     parella.push(this.textContent); //Amb 'this' selecciono l'element clicat.
     quadratsSeleccionats.push(this);
     this.style.color = 'white';
@@ -34,23 +37,35 @@ function comprovarParella(){
     this.style.pointerEvents = 'none'; //He trobat aquesta propietat amb la que, quan una casella està clicada, no es pugui tornar a clicar.
     if(parella.length == 2){ //Quan hem seleccionat el segon element...
         if(parella[0] == parella[1]){
+            numPuntsObj.textContent = parseInt(numPuntsObj.textContent) + 10; //SUMA 10 PUNTS SI ES CORRECTE
+            console.log(numPuntsObj.textContent);
             lletresJugades.push(parella);
             console.log('lletres' + lletresJugades);
             for(i=0; i<parella.length; i++){ //Bucle per canviar el color de les caselles si son iguals.
                 quadratsSeleccionats[i].style.backgroundColor = 'green';
             }
+            parella = [];
+            quadratsSeleccionats = [];
         }else if(parella[0] != parella[1]){
-            quadratsSeleccionats[0].style.pointerEvents = 'auto'; //Si no son iguals torno a activar el click.
-            quadratsSeleccionats[1].style.pointerEvents = 'auto';
-            quadratsSeleccionats[0].style.color = 'blue';
-            quadratsSeleccionats[1].style.color = 'blue';           
-        }
-        parella = [];
-        quadratsSeleccionats = [];
+            if(parseInt(numPuntsObj.textContent)>=3){
+                numPuntsObj.textContent = parseInt(numPuntsObj.textContent) -3; //RESTA 3 PUNTS SI ES INCORRECTE.
+            }
+            quadratsSeleccionats[0].style.backgroundColor = 'red';
+            quadratsSeleccionats[1].style.backgroundColor = 'red';
+            setTimeout(() =>{ //TEMPORITZADOR: Torna les caselles al seu color inicial al cap d'un segon.
+                quadratsSeleccionats[0].style.backgroundColor = 'blue';
+                quadratsSeleccionats[1].style.backgroundColor = 'blue';
+                quadratsSeleccionats[0].style.pointerEvents = 'auto'; //Si no son iguals torno a activar el click.
+                quadratsSeleccionats[1].style.pointerEvents = 'auto';
+                quadratsSeleccionats[0].style.color = 'blue';
+                quadratsSeleccionats[1].style.color = 'blue';
+                parella = [];
+                quadratsSeleccionats = [];
+            }, 1000);                       
+        }        
     }
 }
 
 function mostrarInstruccions(){
-    winInstr = window.open("instruccions.html");
-    winInstr;
+    window.open('instruccions.html', '_blank', 'width=400, height=400');
 }
